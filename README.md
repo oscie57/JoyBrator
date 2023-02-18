3b3a682baad8bd7d3aca553f74d6660b4926d13b
Oscar Hudson <hudso009@gmail.com>
2023-02-18 05:35:02 +0000

    works!

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # JoyBrator
 
- A tool that makes the Joy-Con (HAC-016) endlessly vibrate
+ A tool that makes the Joy-Con endlessly vibrate
 
 ## Building
 
-Using the provided Makefile, just run `make`. This is assuming you have devKitNX installed.
+Using the provided Makefile, just run `make`. This is assuming you have libNX setup.
 
 ## Controls
 
@@ -12,21 +12,26 @@ Using the provided Makefile, just run `make`. This is assuming you have devKitNX
 | :--------- | :--------------- |
 | `A`        | Toggle rumble    |
 | `B`        | Toggle pulse     |
-| `+`        | Toggle pulse     |
-| `Home`     | Exit application |
+| `+`        | Exit application |
 
-Third-Party GameCube controllers are confirmed to work.
+These controllers are confirmed and tested to work:
+
+- Joy-Con (HAC-016)
+- Pro Controller (HAC-013)
 
 ## ⚠️ DISCLAIMER ⚠️
 
 DISCLAIMER: To avoid injury or aggravation of pre-existing conditions,
 the vibrating remote should not be used on swollen or inflamed areas
 or skin lacerations. No medical claims are warranted or implied by
-the use of this software. Exessive use of your vibrating remote
-will wear out the motor.
+the use of this software. Vibrating your remote for a long period of
+time may (and probably will) damage the motor. Please be careful
+and take breaks in between use to ensure safety of the HD rumble.
 
 ## Credits
 
 TeamDwiildo - Original inspiration for this project.
 
-[UntitledOutput](https://twitter.com/UntitledOutput) - Giving me motivation to work on this
+[UntitledOutput](https://github.com/UntitledOutput) - Giving me motivation to work on this
+
+[shadowninja108](https://github.com/shadowninja108) - Assistance with libNX
--- source/main.c
+++ source/main.c
@@ -9,6 +9,13 @@
 // Main program entrypoint
 int main(int argc, char* argv[])
 {
+    HidVibrationDeviceHandle VibrationDeviceHandles[2];
+    Result rc = 0, rc2 = 0;
+
+    HidVibrationValue VibrationValue;
+    HidVibrationValue VibrationValue_stop;
+    HidVibrationValue VibrationValues[2];
+
     // This example uses a text console, as a simple way to output text to the screen.
     // If you want to write a software-rendered graphics application,
     //   take a look at the graphics/simplegfx example, which uses the libnx Framebuffer API instead.
@@ -26,39 +33,46 @@ int main(int argc, char* argv[])
     // Other initialization goes here. As a demonstration, we print hello world.
 
     printf("\n");
-
     printf("\n  JoyBrator v1.0");
-    printf("\n  (c)2023 oscie57, UntitledOutput");
-
+    printf("\n  (c)2023 oscie57, UntitledOutput, shadowninja108");
     printf("\n\n");
-
     printf("\n  DISCLAIMER: To avoid injury or aggravation of pre-existing conditions,");
     printf("\n  the vibrating remote should not be used on swollen or inflamed areas or");
     printf("\n  skin lacerations. No medical claims are warranted or implied by the use");
-    printf("\n  of this software. Exessive use of your vibrating remote will wear out");
-    printf("\n  the motor.");
-
+    printf("\n  of this software. Vibrating your remote for a long period of time may");
+    printf("\n  (and probably will) damage the motor. Please be careful and take breaks");
+    printf("\n  breaks in between use to ensure safety of the HD rumble.");
     printf("\n\n\n\n");
-
-    printf("\n  Supported controllers: Joy-Con (HAC-016)");
-
+    printf("\n  Supported/tested controllers: Joy-Con (HAC-016), Pro Controller (HAC-013)");
     printf("\n\n");
-
     printf("\n  Press 'A' to enable/disable rumble");
     printf("\n  Press 'B' to enable/disable pulse mode");
-    printf("\n  Press '+' to toggle the LED");
-
     printf("\n\n");
-
-    printf("\n  Press 'Home' when you're done");
-
+    printf("\n  Press '+' when you're done");
     printf("\n\n\n");
-
     printf("\n  PULSE COMING SOON!");
-
     printf("\n\n\n\n\n\n\n\n\n\n\n\n\n");
-
     printf("\n  https://github.com/oscie57/JoyBrator");
+
+    // Two VibrationDeviceHandles are returned: first one for left-joycon, second one for right-joycon.
+    // Change the total_handles param to 1, and update the hidSendVibrationValues calls, if you only want 1 VibrationDeviceHandle.
+    rc = hidInitializeVibrationDevices(VibrationDeviceHandles, 2, HidNpadIdType_No1, HidNpadStyleTag_NpadJoyDual);
+
+    // Setup VibrationDeviceHandles for HidNpadIdType_No1 too, since we want to support both HidNpadIdType_Handheld and HidNpadIdType_No1.
+
+    VibrationValue.amp_low   = 1.0f;
+    VibrationValue.freq_low  = 10.0f;
+    VibrationValue.amp_high  = 1.0f;
+    VibrationValue.freq_high = 10.0f;
+
+    memset(VibrationValues, 0, sizeof(VibrationValues));
+
+    memset(&VibrationValue_stop, 0, sizeof(HidVibrationValue));
+    // Switch like stop behavior with muted band channels and frequencies set to default.
+    VibrationValue_stop.freq_low  = 160.0f;
+    VibrationValue_stop.freq_high = 320.0f;
+
+    bool rumbling = false;
 
     // Main loop
     while (appletMainLoop())
@@ -70,16 +84,50 @@ int main(int argc, char* argv[])
         // newly pressed in this frame compared to the previous one
         u64 kDown = padGetButtonsDown(&pad);
 
-        if (kDown & HidNpadButton_Plus)
+        if (kDown & HidNpadButton_Plus) {
             break; // break in order to return to hbmenu
+        };
 
+        if (R_SUCCEEDED(rc) && (kDown & HidNpadButton_A)) {
+
+            if ( rumbling ) {
+
+                rumbling = false;
+
+                memcpy(&VibrationValues[0], &VibrationValue_stop, sizeof(HidVibrationValue));
+                memcpy(&VibrationValues[1], &VibrationValue_stop, sizeof(HidVibrationValue));
+
+                rc2 = hidSendVibrationValues(VibrationDeviceHandles, VibrationValues, 2);
+
+                //Could also do this with 1 hidSendVibrationValues() call + a larger VibrationValues array.
+                rc2 = hidSendVibrationValues(VibrationDeviceHandles, VibrationValues, 2);
+
+            } else {
+
+                rumbling = true;
+
+                //Calling hidSendVibrationValue/hidSendVibrationValues is really only needed when sending new VibrationValue(s).
+                //If you just want to vibrate 1 device, you can also use hidSendVibrationValue.
+
+                memcpy(&VibrationValues[0], &VibrationValue, sizeof(HidVibrationValue));
+                memcpy(&VibrationValues[1], &VibrationValue, sizeof(HidVibrationValue));
+                
+                rc2 = hidSendVibrationValues(VibrationDeviceHandles, VibrationValues, 2);
+                if (R_FAILED(rc2)) printf("hidSendVibrationValues() returned: 0x%x\n", rc2);
+
+            };
+        };
+
+        if (kDown & HidNpadButton_B) {
+            // Add pulse later
+        };
+        
         // Your code goes here
 
         // Update the console, sending a new frame to the display
         consoleUpdate(NULL);
     }
 
-    // Deinitialize and clean up resources used by the console (important!)
     consoleExit(NULL);
     return 0;
 }
